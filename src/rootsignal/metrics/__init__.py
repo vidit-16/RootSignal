d1a2859199e6fe37407ed23743bfb09bf3a41663
vidit choudhary <153429790+vidit-16@users.noreply.github.com>
2026-09-10 03:07:11 +0530

    Add KPI metrics package

---
+++ src/rootsignal/metrics/__init__.py
@@ -0,0 +1,17 @@
+from .kpis import (
+    add_growth,
+    calculate_order_kpis,
+    calculate_sales_kpis,
+    calculate_target_variance,
+    calculate_fill_rate,
+    calculate_primary_secondary_mix,
+)
+
+__all__ = [
+    "add_growth",
+    "calculate_order_kpis",
+    "calculate_sales_kpis",
+    "calculate_target_variance",
+    "calculate_fill_rate",
+    "calculate_primary_secondary_mix",
+]
